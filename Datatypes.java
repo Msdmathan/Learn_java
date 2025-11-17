8289bc6eb41b5aec8492ffeec1f9438338120f23
Msdmathan <89250756+Msdmathan@users.noreply.github.com>
2025-11-17 21:04:40 +0530

    Add Datatypes class with examples of Java types
    The data types in java is here.

---
+++ Datatypes.java
@@ -0,0 +1,25 @@
+class Datatypes{
+    public static void main(String[] args){
+
+        // Integer data type
+        int myNum = 5;
+        System.out.println("Integer: " + myNum);
+
+        // Floating point data type
+        float myFloatNum = 5.99f;
+        System.out.println("Float: " + myFloatNum);
+
+        // Character data type
+        char myLetter = 'D';
+        System.out.println("Character: " + myLetter);
+
+        // String data type
+        String myText = "Hello World";
+        System.out.println("String: " + myText);
+
+        //Boolean data type
+        boolean myBool = true;
+        System.out.println("Boolean: " + myBool);
+
+    }
+}
