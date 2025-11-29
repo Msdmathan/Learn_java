6000a13965c89785adf4afdea8afc02e6233c9ff
Msdmathan <89250756+Msdmathan@users.noreply.github.com>
2025-11-29 23:12:32 +0530

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 public class RockPaperScissors{
   RockPaperScissors(){
-    
+    String playerSelection;
+    String computerSelection;
+    String playAgain="yes";
   }
-  public static void main(Steing[] args){
+  public static void main(String[] args){
     
   }
 }
