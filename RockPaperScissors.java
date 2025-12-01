4f965526ac70a2a69d9d9f23f5ce2875985bb0fb
Msdmathan <89250756+Msdmathan@users.noreply.github.com>
2025-12-01 21:24:49 +0530

    Add Random import for computer selection

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.Random;
 public class RockPaperScissors{
   RockPaperScissors(){
     String playerSelection;
     String computerSelection;
     String playAgain="yes";
+    Random random = new Random();
   }
   public static void main(String[] args){
     
